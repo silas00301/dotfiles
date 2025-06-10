db4225015e49e96780b30f733050ffa20859a17c
Silas <contact@silash.dev>
2025-06-10 17:50:09 +0200

    add jj starship module

--- home/shared/programs/starship.nix
+++ home/shared/programs/starship.nix
@@ -96,6 +96,43 @@
         Void = " ";
         Windows = "󰍲 ";
       };
+      git_state.disabled = true;
+      git_commit.disabled = true;
+      git_metrics.disabled = true;
+      git_branch.disabled = true;
+      custom = {
+        jj = {
+          ignore_timeout = true;
+          description = "The current jj status";
+          detect_folders = [".jj"];
+          symbol = " ";
+          command = ''
+            jj log --revisions @ --no-graph --ignore-working-copy --color always --limit 1 --template '
+              separate(" ",
+                change_id.shortest(4),
+                bookmarks,
+                "|",
+                concat(
+                  if(conflict, "💥"),
+                  if(divergent, "🚧"),
+                  if(hidden, "👻"),
+                  if(immutable, "🔒"),
+                ),
+                raw_escape_sequence("\x1b[1;32m") ++ if(empty, "(empty)"),
+                raw_escape_sequence("\x1b[1;32m") ++ coalesce(
+                  truncate_end(29, description.first_line(), "…"),
+                  "(no description set)",
+                ) ++ raw_escape_sequence("\x1b[0m"),
+              )
+            '
+          '';
+        };
+        git_branch = {
+          when = true;
+          description = "Only show git_branch if we're not in a jj repo";
+          command = "jj root > /dev/null 2>&1 || starship module git_branch";
+        };
+      };
     };
   };
 }
