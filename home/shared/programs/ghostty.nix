45f61e6c0cd72b31ba895382689372bf582b9d12
Silas <contact@silash.dev>
2025-06-19 19:03:17 +0200

    ghostty uses configured catppuccin flavor now

--- home/shared/programs/ghostty.nix
+++ home/shared/programs/ghostty.nix
@@ -5,7 +5,6 @@
     enableFishIntegration = true;
     enableZshIntegration = true;
     settings = {
-      theme = "catppuccin-mocha";
       background-opacity = 0.9;
       font-family = "Maple Mono NF CN";
       background-blur = true;
