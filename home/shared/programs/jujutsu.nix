822512398d76db9fa94965f9a51241982e8782ef
Silas <contact@silash.dev>
2025-07-07 13:07:07 +0200

    add tug alias to jj

--- home/shared/programs/jujutsu.nix
+++ home/shared/programs/jujutsu.nix
@@ -7,11 +7,13 @@
         email = "contact@silash.dev";
       };
       ui.default-command = "log";
-      ui.paginate = "never";
       signing = {
         behavior = "own";
         backend = "ssh";
         key = "~/.ssh/id_rsa_private.pub";
+      };
+      aliases = {
+        tug = ["bookmark" "move" "--from" "heads(::@ & bookmarks())" "--to" "@"];
       };
     };
   };
